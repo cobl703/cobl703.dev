296a302d58619500e3a05e756cfe52301c5f0fdf
cobl703 <98135295+cobl703@users.noreply.github.com>
2025-01-06 22:15:12 -0500

    Add files via upload

---
+++ js/navbargen.js
@@ -0,0 +1,14 @@
+navbar = `
+<nav id="navbar">
+	<div class="navbar-left">
+		<img src="/assets/logo.png">
+		<p>cobl703</p>
+	</div>
+	<div class="navbar-right">
+		<a href="/">Home</a>
+		<a href="https://github.com/cobl703/cobl703">Github</a>
+	</div>
+</nav>
+`;
+
+document.body.insertAdjacentHTML("afterbegin", navbar);
